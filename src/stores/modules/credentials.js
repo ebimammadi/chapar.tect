a5abec00f33df623abf79c2a86963c7477ee4c66
ebimammadi <ebi@tadbir.net>
2020-06-04 16:54:41 +0200

    a file to replicate the login

---
+++ src/stores/modules/credentials.js
@@ -0,0 +1,26 @@
+const state = {
+  signed: false,
+}
+
+const getters = {
+  isSinged: state => state.signed,
+}
+
+const actions = {
+  setSingInStatus({ commit }, value) {
+    commit('changeSingInStatus', value)
+  },
+  
+}
+
+const mutations = {
+  changeSingInStatus: (state, value) => (state.signed = value),
+  
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}

76bd10ab836c708cc4b32703612e87cd6ace225a
ebimammadi <ebi@tadbir.net>
2020-06-25 09:27:26 +0200

    update croppie

--- src/components/ImageUpload.vue
+++ src/components/ImageUpload.vue
@@ -1,46 +1,14 @@
 <template>
   <div>
-    <!-- Note that 'ref' is important here (value can be anything). read the description about `ref` below. -->
-    <!-- <vue-croppie
-        ref="croppieRef"
-        :enableOrientation="true"
-        :boundary="{ width: 300, height: 300}"
-        @result="result"
-        @update="update">
+    <input type="file" @change="croppie"/>
+    <vue-croppie ref="croppieRef" :enableOrientation="true" :boundary="{ width: 450, height: 300}" :viewport="{ width:400, height:250, 'type':'square' }">
     </vue-croppie>
-
     <!-- the result -->
-    <!-- <img v-bind:src="cropped">
-
-    <button @click="bind()">Bind</button>
-    <!-- Rotate angle is Number -->
-    <!-- <button @click="rotate(-90)">Rotate Left</button>
-    <button @click="rotate(90)">Rotate Right</button>
-    <button @click="crop()">Crop Via Callback</button>
-    <button @click="cropViaEvent()">Crop Via Event</button> -->
-
-
-    <!-- <input type="file" @change="croppie" v-bind:name="file"/> -->
-    <b-form-file
-      v-model="file"
-      :state="Boolean(file)"
-      placeholder="Choose a file or drop it here..."
-      drop-placeholder="Drop file here..."
-      @change="croppie"
-    ></b-form-file>
-    <div v-if="file">
-      <vue-croppie
-        ref="croppieRef"
-        :enableOrientation="true"
-        :boundary="{ width: 450, height: 300}"
-        :viewport="{ width:400, height:250, 'type':'square' }">
-      </vue-croppie>
-      <!-- the result -->
-      <img :src="cropped"><br/>
-      <button @click="crop">Crop</button>
-    </div>
+    <img :src="cropped">
+    <button @click="crop">Crop</button>
+    <button @click="h">test</button>
+    <!-- <button @click="rotate">Rotate</button> -->
   </div>
-
 </template>
 
 <script>
@@ -54,11 +22,13 @@ export default {
   data() {
     return {
       croppieImage: '',
-      cropped: null,
-      file: null
+      cropped: null
     };
   },
   methods: {
+    h(){
+      console.log(`test`);this.$refs.croppieRef.rotate(90);
+    },
     croppie (e) {
       var files = e.target.files || e.dataTransfer.files;
       if (!files.length) return;
@@ -85,69 +55,10 @@ export default {
           console.log(this.croppieImage);
         });
       }
-  }
+    },
+    rotate() {
+      this.$refs.croppieRef.rotate(90);
+    }
 };
-// export default {
-//   mounted() {
-//       // Upon mounting of the component, we accessed the .bind({...})
-//       // function to put an initial image on the canvas.
-//       this.$refs.croppieRef.bind({
-//           url: 'http://i.imgur.com/Fq2DMeH.jpg',
-//       })
-//   },
-//   data() {
-//       return {
-//           cropped: null,
-//           images: [
-//               'http://i.imgur.com/fHNtPXX.jpg',
-//               'http://i.imgur.com/ecMUngU.jpg',
-//               'http://i.imgur.com/7oO6zrh.jpg',
-//               'http://i.imgur.com/miVkBH2.jpg'
-//           ]
-//       }
-//   },
-//   methods: {
-//       bind() {
-//           // Randomize cat photos, nothing special here.
-//           let url = this.images[Math.floor(Math.random() * 4)]
 
-//           // Just like what we did with .bind({...}) on
-//           // the mounted() function above.
-//           this.$refs.croppieRef.bind({
-//               url: url,
-//           });
-//       },
-//       // CALBACK USAGE
-//       crop() {
-//           // Here we are getting the result via callback function
-//           // and set the result to this.cropped which is being
-//           // used to display the result above.
-//           let options = {
-//               format: 'jpeg',
-//               circle: true
-//           }
-//           this.$refs.croppieRef.result(options, (output) => {
-//               this.cropped = output;
-//           });
-//       },
-//       // EVENT USAGE
-//       cropViaEvent() {
-//         let options = {
-//               format: 'png',
-//               circle: true
-//           }
-//           this.$refs.croppieRef.result(options);
-//       },
-//       result(output) {
-//           this.cropped = output;
-//       },
-//       update(val) {
-//           console.log(val);
-//       },
-//       rotate(rotationAngle) {
-//           // Rotates the image
-//           this.$refs.croppieRef.rotate(rotationAngle);
-//       }
-//   }
-// }
 </script>

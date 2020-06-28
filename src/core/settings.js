2dd6b1d0aced6c1e2af9a493df90e5381e304f1d
ebimammadi <ebi@tadbir.net>
2020-06-28 14:27:33 +0200

    axios_timeout set to 10seconds

--- src/core/settings.js
+++ src/core/settings.js
@@ -2,7 +2,7 @@ const APP_SETTINGS = {
     app_name: 'chapar.tech',
     app_url: 'https://chapar.tech/',
     remote_api_base_url: 'http://localhost:8080', //path to the remote backend
-    axios_timeout: 5000, //milisoconds to wait for the backend
+    axios_timeout: 10000, //milisoconds to wait for the backend
     app_version: '1.1.2'
 }
 

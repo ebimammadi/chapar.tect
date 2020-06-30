a7ec33646b2a71f799e05566e8ece9ecc540598e
ebimammadi <ebi@tadbir.net>
2020-06-30 19:02:16 +0200

    added address view as a test

---
+++ src/views/users/Address.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>address ...</h1>
+  </div>
+</template>
+
+<script>
+import ApiService from "@/core/ApiService";
+import Store from "@/stores/stores";
+
+export default {
+  name: "About",
+  methods: {},
+
+  created() {
+    ApiService.get("/about")
+      .then(response => {
+        Store.commit("changeMessage", `${response.data.message}`);
+        Store.commit("changeVariant", "success");
+      })
+      .catch(err => {
+        console.log(err);
+        console.log("error in about/validate page");
+        if (!err.status) Store.commit("changeMessage", "Network Error!");
+      })
+      .finally(function() {
+        //test
+        //Store.commit('changeOverlayShow', false);
+      });
+  }
+};
+</script>

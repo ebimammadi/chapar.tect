51c255795ddaffe3274fd97fe85ef226b322a98e
ebimammadi <ebi@tadbir.net>
2020-07-01 09:36:47 +0200

    code refactor

--- src/components/BreadCrumb.vue
+++ src/components/BreadCrumb.vue
@@ -26,40 +26,6 @@ export default {
       }, [])
       breadcrumbs.unshift({ text: 'Home', to: {name: 'home'}})
       return breadcrumbs;
-      // const breadcrumb = [];
-      // if (pathArray.length == 1 && pathArray[0] == "") return breadcrumb;//!no need to add home
-      // //console.log(`patharray:`,pathArray)
-
-      // for (let i = 0; i<pathArray.length; i++ ) {
-      //   //console.log('look');
-      //   //console.log(this.$route.matched[i])
-      //   if (this.$route.matched[0]){
-      //     let item = {};
-      //     const route = this.$route.matched[i];
-      //     console.log(this.$route.matched[0].children);
-      //     if (this.$route.matched[0].children != null) {
-      //       console.error('yes');
-      //     }
-      //     if (('children' in route) ){
-      //       console.log(this.$route.matched[0].children)
-      //       item = {
-      //         text: capitalize(this.$route.matched[i]),
-      //         to: { name: this.$route.matched[i].name },
-      //       };
-      //     }
-      //     else
-      //       item = {
-      //         text: capitalize(this.$route.matched[i].name),
-      //         to: { name: this.$route.matched[i].name },
-      //       };
-      //       if (i == pathArray.length-1) { item.active = true; item.to = null }
-      //       breadcrumb.push(item)
-      //   }
-
-      // }
-      // breadcrumb.unshift({ text: 'Home', to: {name: 'home'}})
-      // //console.log(`breadcrumb`,breadcrumb)
-      // return breadcrumb;
     }
   }
 };

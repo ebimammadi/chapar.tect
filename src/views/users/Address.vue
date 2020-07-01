3f630a586204901d40c7ce58a5d8c6de8826bfbd
ebimammadi <ebi@tadbir.net>
2020-07-01 12:00:37 +0200

    refactor

--- src/views/users/Address.vue
+++ src/views/users/Address.vue
@@ -5,28 +5,28 @@
 </template>
 
 <script>
-import ApiService from "@/core/ApiService";
-import Store from "@/stores/stores";
+// import ApiService from "@/core/ApiService";
+// import Store from "@/stores/stores";
 
 export default {
   name: "About",
   methods: {},
 
   created() {
-    ApiService.get("/about")
-      .then(response => {
-        Store.commit("changeMessage", `${response.data.message}`);
-        Store.commit("changeVariant", "success");
-      })
-      .catch(err => {
-        console.log(err);
-        console.log("error in about/validate page");
-        if (!err.status) Store.commit("changeMessage", "Network Error!");
-      })
-      .finally(function() {
-        //test
-        //Store.commit('changeOverlayShow', false);
-      });
+    // ApiService.get("/about")
+    //   .then(response => {
+    //     Store.commit("changeMessage", `${response.data.message}`);
+    //     Store.commit("changeVariant", "success");
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //     console.log("error in about/validate page");
+    //     if (!err.status) Store.commit("changeMessage", "Network Error!");
+    //   })
+    //   .finally(function() {
+    //     //test
+    //     //Store.commit('changeOverlayShow', false);
+    //   });
   }
 };
 </script>

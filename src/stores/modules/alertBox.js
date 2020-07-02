a81168fe1438860548e6201c27916aca3fd639a8
ebimammadi <ebi@tadbir.net>
2020-07-02 18:35:19 +0200

    alertChange mutation added

--- src/stores/modules/alertBox.js
+++ src/stores/modules/alertBox.js
@@ -15,11 +15,19 @@ const actions = {
   setVariant ({ commit }, value) {
     commit('changeVariant', value)
   },
+  setChangeAlert ( { commit }, message, variant) {
+    commit('changeAlert', message, variant)
+  }
 }
 
 const mutations = {
   changeMessage: (state, value) => (state.message = value),
-  changeVariant: (state, value) => (state.variant = value)
+  changeVariant: (state, value) => (state.variant = value),
+  changeAlert: (state, message, variant) => {
+    state.variant = variant;
+    state.message = message;
+    document.getElementsByClassName('alert-box')[0].scrollIntoView();
+  }
 }
 
 export default {

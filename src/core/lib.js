07d8f24fb982897a9d19a0804aceb335cca7f8f7
ebimammadi <ebi@tadbir.net>
2020-07-07 16:21:32 +0200

    validateEmail added

--- src/core/lib.js
+++ src/core/lib.js
@@ -13,8 +13,8 @@ const validateURL = str => {
 };
 
 function validateEmail(email) {
-  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const regex = "/^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/";
   return regex.test(String(email).toLowerCase());
 }
 
-export default { validateURL, validateEmail };
+export { validateURL, validateEmail };

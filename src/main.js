df19bce5b4464de1f482f1985afe98b67d39dee8
ebimammadi <ebi@tadbir.net>
2020-07-07 12:46:54 +0200

    refactor

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
 
-import "@/plugins/bootstrap-vue.js";
-import "@/assets/custom.scss";
-
 import router from "@/router/router.js";
 import store from "@/store/index.js";
 import ApiService from "@/core/ApiService.js";
 ApiService.init();
 
 //other plugins would be added here...
+import "@/plugins/bootstrap-vue.js";
+import "@/assets/custom.scss";
+
+//import "@/plugins/vuelidate.js";
 
 Vue.config.productionTip = false;
 

9587d0efbf87b1badf5c9c9af9666707ee755d10
ebimammadi <ebi@tadbir.net>
2020-07-08 20:31:53 +0200

    refactor

--- src/core/lib.js
+++ src/core/lib.js
@@ -1,4 +1,4 @@
-import * as EmailValidator from 'email-validator';
+import * as EmailValidator from "email-validator";
 
 const validateURL = str => {
   try {
@@ -9,9 +9,8 @@ const validateURL = str => {
   return true;
 };
 
-const validateEmail = (email) => EmailValidator.validate(email)
-  // const regex = `/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/`;
-  // return regex.test(String(email).toLowerCase());
-
+const validateEmail = email => EmailValidator.validate(email);
+// const regex = `/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/`;
+// return regex.test(String(email).toLowerCase());
 
 export { validateURL, validateEmail };

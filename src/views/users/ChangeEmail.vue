da3b74e131f31b9722e3f39d96279b70aa4508ce
Ebrahim Mohammadi <19ebmo01@hig.se>
2020-07-12 10:31:22 +0200

    minor bug on computed property

--- src/views/users/ChangeEmail.vue
+++ src/views/users/ChangeEmail.vue
@@ -72,7 +72,7 @@ export default {
   methods: {
     ...mapActions(["setAlert"]),
     changeEmail() {
-      if (!this.validateNewEmail(this.email))
+      if (!this.validateNewEmail)
         return this.setAlert({ message: this.validation.email });
       if (this.password.length < 8)
         return this.setAlert({ message: `Your password seems invalid!` });

f10b4a22e5f6670ec433b2632a4702ebe8d95b99
Ebrahim Mohammadi <19ebmo01@hig.se>
2020-07-12 10:30:57 +0200

    refactor

--- src/core/settings.js
+++ src/core/settings.js
@@ -1,7 +1,7 @@
 const APP_SETTINGS = {
   app_name: "chapar.tech",
   app_url: "https://chapar.tech/",
-  remote_api_base_url: "http://localhost:8080", //path to the remote backend
+  remote_api_base_url: "https://chapar-tech-api.herokuapp.com/", //"http://localhost:8080", //path to the remote backend
   axios_timeout: 10000, //milisoconds to wait for a backend request's response
   app_version: "1.1.2"
 };

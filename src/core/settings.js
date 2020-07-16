f9afa255bed419c6dbc8f0436a717d1d574e3f51
Ebrahim Mohammadi <19ebmo01@hig.se>
2020-07-16 14:17:37 +0200

    set to local IP

--- src/core/settings.js
+++ src/core/settings.js
@@ -1,6 +1,6 @@
 const APP_SETTINGS = {
   app_name: "chapar.tech",
-  app_url: "https://chapar.tech/",
+  app_url: "http://130.243.7.147",
   remote_api_base_url: "https://chapar-tech-api.herokuapp.com/", //"http://localhost:8080", //path to the remote backend
   axios_timeout: 10000, //milliseconds to wait for a backend request's response
   app_version: "1.1.2"

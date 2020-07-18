cfebfe94a43ce25a705ad7dfc36984cbf7e39ef0
ebimammadi <ebi@tadbir.net>
2020-07-18 10:36:38 +0200

    UsersLogs init default

---
+++ src/views/app/users/UsersLogs.vue
@@ -0,0 +1,10 @@
+<template>
+  <b-container>
+    <b-row class="mb-3">
+      <b-col>
+        <h1>Logs Records</h1>
+        <p>Under construction</p>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>

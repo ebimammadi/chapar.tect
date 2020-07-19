13b713abfdbeb00006f8cac8fbb521ec58b33da4
ebimammadi <ebi@tadbir.net>
2020-07-19 16:47:43 +0200

    timeout set to 20 seconds

--- src/core/settings.js
+++ src/core/settings.js
@@ -2,7 +2,7 @@ const APP_SETTINGS = {
   app_name: "chapar.tech",
   app_url: "http://130.243.7.147",
   remote_api_base_url: "https://chapar-tech-api.herokuapp.com/", //"http://localhost:8080", //path to the remote backend
-  axios_timeout: 10000, //milliseconds to wait for a backend request's response
+  axios_timeout: 20000, //milliseconds to wait for a backend request's response
   app_version: "1.1.2"
 }
 

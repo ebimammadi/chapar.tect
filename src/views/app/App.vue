d0bf6d4cdc9503d3576df861f1b79d7e1a6d48ec
Ebrahim Mohammadi <19ebmo01@hig.se>
2020-07-19 10:58:50 +0200

    new app home

--- src/views/app/App.vue
+++ src/views/app/App.vue
@@ -1,3 +1,12 @@
 <template>
-  <div>TEST</div>
+  <b-container>
+    <b-row class="mb-3">
+      <b-col>
+        <h1>
+          Welcome to the Application Panel!
+        </h1>
+        <p><br><br>The new features would be added to the application Panel.</p>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>

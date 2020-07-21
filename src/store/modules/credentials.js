2df8584f1b8da94ae0d317964ced5ddf85fb11b7
ebimammadi <ebi@tadbir.net>
2020-07-21 16:12:58 +0200

    comment removed

--- src/store/modules/credentials.js
+++ src/store/modules/credentials.js
@@ -7,24 +7,19 @@ const state = {
 const getters = {
   isSinged: state => state.signed,
   userInfo: state => state.userInfo,
-  // profilePhotoUrl: state => state.profilePhotoUrl
-  profilePhotoUrl: state => {
-    console.log(`yes from getter function`,state.profilePhotoUrl)
-    return state.profilePhotoUrl//window.localStorage.getItem("profilePhotoUrl")
-  }
+  profilePhotoUrl: state => state.profilePhotoUrl
 }
 
 const actions = {
   setSingInStatus({ commit }, value) { commit("changeSingInStatus", value) },
   setUserInfo( { commit }, value) { commit("changeUserInfo", value) },
-  SetProfilePhotoUrl( { commit }, value ) { commit("changeProfilePhotoUrl", value )}
+  setProfilePhotoUrl( { commit }, value ) { commit("changeProfilePhotoUrl", value )}
 }
 
 const mutations = {
   changeSingInStatus: (state, value) => (state.signed = value),
   changeUserInfo: (state, value) => (state.userInfo = value),
   changeProfilePhotoUrl: (state, value) => {
-    console.log(`we are mutating!!!!!!!`)
     window.localStorage.setItem("profilePhotoUrl", value)
     state.profilePhotoUrl = value
   }

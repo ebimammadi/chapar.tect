d16f5dd35e7b98f0eef39ef150e5fa56b5a97b49
ebimammadi <ebi@tadbir.net>
2020-07-23 19:40:52 +0200

    init

---
+++ src/components/ModalConfirm.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-modal 
+    id="mainModal" 
+    ref="mainModal"
+    @ok="ok"
+    @cancel="cancel"
+    :title="title" 
+    :_id="_id" 
+  >
+    <p v-html="body"></p>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { default: "" }, 
+    body: { default: "" }, 
+    _id: { default: "" } 
+  },
+  methods: {
+    ok(){
+      this.$emit('ok',{ _id: this._id })
+    },
+    cancel() {
+      this.$emit('cancel',{ _id: this._is })
+    }
+  }
+}
+</script>

9110899ccb9dcd3b2eb3f5fe3d363ed7e804399c
ebimammadi <ebi@tadbir.net>
2020-07-25 16:35:05 +0200

    improved

--- src/views/app/users/UsersList.vue
+++ src/views/app/users/UsersList.vue
@@ -3,18 +3,16 @@
     <b-row class="mb-3">
       <b-col>
         <h1 class="mb-3">User list</h1>
-        <!-- <p v-b-tooltip.hover title="Registration Time" class="mt-2">
-              <b-icon-clock> </b-icon-clock> {{ data.value.date }}
-            </p> -->
         <b-table 
-          ref="userTable"
           responsive
           striped
           hover
           stacked="sm"
           :items="users"
           :fields="fields"
+          :class="'table'"
         >
+          <!-- userSlot column -->
           <template v-slot:cell(nameSlot)="data">
             <b-avatar :src="data.item.profilePhotoUrl" />
             {{ data.item.name }}
@@ -23,64 +21,70 @@
               v-b-tooltip.hover title="View Profile"
               :to="{ name: 'user profile', params: { user: data.item.email } }"
             >
-                <b-icon-eye />                  
+              <b-icon-person />                  
+            </router-link>
+            <router-link v-if="data.item.userRole != 'user' "
+              variant="primary" 
+              v-b-tooltip.hover title="View slug"
+              :to="{ name: 'supplier slug', params: { user: data.item.email } }"
+            >
+              <b-icon-eye />                  
             </router-link>
           </template>
           <!-- EmailSlot column -->
-          <template v-slot:cell(emailSlot)="data">
-            <span v-b-tooltip.hover :title="data.value.emailTooltip" >
-              {{ data.value.email }} 
+          <template v-slot:cell(emailSlot)="data" v-bind:style="{ verticalAlign: 'middle' }">
+            <span v-b-tooltip.hover :title="data.value.emailTooltip">
               <b-icon-check-all 
                 v-if="data.value.emailVerify" 
                 variant="success"
-              > 
-              </b-icon-check-all>
+              /> 
               <b-icon-exclamation-circle 
                 v-if="!data.value.emailVerify" 
                 variant="warning"
-              > 
-              </b-icon-exclamation-circle>
+              /> {{ data.value.email }} 
             </span> 
-            
           </template>
+          <!-- MobileSlot column -->
           <template v-slot:cell(mobileSlot)="data">
             <span v-if="data.value.mobile.length>0" v-b-tooltip.hover :title="data.value.mobileTooltip" >
               {{ data.value.mobile }} 
               <b-icon-check-all 
                 v-if="data.value.mobileVerify" 
                 variant="success"
-              > 
-              </b-icon-check-all>
+              /> 
               <b-icon-exclamation-circle 
                 v-if="!data.value.mobileVerify" 
                 variant="warning"
-              > 
-              </b-icon-exclamation-circle>
+              /> 
             </span>
           </template>
-          <!-- <template v-slot:cell(role)="data">
-            <p>{{ data.value }}</p>
-          </template> -->
+          <template v-slot:cell(userRole)="data">
+            {{ data.item.userRole | titleize }}
+          </template>
           <!-- status column -->
           <template v-slot:cell(status)="data">
-            {{ data.value.status }}
-            <b-button @click="userActivateShowModal(data.value._id)"
-              :variant=" data.value.status == `Active`? `outline-warning`: `outline-secondary` "
-              :id="`activation`+ data.value._id" 
-            >
-              <b-tooltip :target="`activation`+ data.value._id" triggers="hover">
-                {{ data.value.status == `Active`? `Suspend User` : `Activate User` }}
-              </b-tooltip>
-              <b-icon-pause v-if="data.value.status == `Active`"> </b-icon-pause>
-              <b-icon-play v-if="data.value.status != `Active`"> </b-icon-play>
+            {{ data.value.status | titleize }}
+            <b-button @click="userActivateShowModal(data.item._id)"
+              variant="link"
+              size="sm"
+              :id="`activation`+ data.item._id" 
+              >
+              <b-icon-pencil />
             </b-button>
+            <b-tooltip :target="`activation`+ data.item._id">
+                {{ data.value.status == `Active`? `Suspend User` : `Activate User` | titleize}}
+            </b-tooltip>
           </template>
           <template v-slot:cell(date)="data">
-            {{ data.value | dateTime }}
+            {{ data.item.date | dateTime }} <b-icon-list-check />
           </template>
         </b-table>
-        <modal-confirm :title="modalTitle" :body="modalBody" :_id="modalId" @ok="userActivateToggle" />
-        <!-- <button v-b-modal="'mainModal'">Show Modal</button> -->
+        <modal-confirm 
+          :title="modal.title" 
+          :body="modal.body" 
+          :_id="modalId" 
+          @ok="userActivateToggle" 
+        />
       </b-col>
     </b-row>
   </b-container>
@@ -88,17 +92,18 @@
 
 <script>
 import ModalConfirm from "@/components/ModalConfirm"
+// import BootstrapToggle from 'vue-bootstrap-toggle'
+
 import ApiService from "@/core/ApiService"
 import { mapActions } from "vuex"
 
 export default {
   components: {
-    ModalConfirm
+    ModalConfirm,
   },
   data(){
     return {
-      modalTitle: "",
-      modalBody: "",
+      modal: {},
       modalId: "",
       usersRaw: [],
       fields: [
@@ -107,7 +112,7 @@ export default {
         { key: "mobileSlot" , label: "Mobile" },  
         { key: "userRole", label: "Role" },
         { key: "status", label: "Status" },
-        { key: "date", label: "Reg. Time"}
+        { key: "date", label: "Reg. Time"},
       ]
     }
   },
@@ -132,9 +137,11 @@ export default {
     userActivateShowModal(_id){
       const item = this.usersRaw.find( item => item._id === _id)
       const action = item.isActive ? `suspend` : `activate`
-      this.modalTitle = item.isActive ? `Suspend User` : `Activate User`
-      this.modalBody = `Are you sure to ${action} user <b>${item.name} (${item.email})</b> ?`
+      this.modal.title = item.isActive ? `Suspend User` : `Activate User`
+      this.modal.body = `Are you sure to ${action} user <b>${item.name} (${item.email})</b>?`
       this.modalId = _id
+      //this.modal._id = _id
+      console.log(this.modal._id)
       this.$root.$emit( 'bv::show::modal', 'mainModal', '#btnShow')
     }
   },
@@ -150,32 +157,21 @@ export default {
         item.emailSlot = {
           email: item.email,
           emailVerify: (item.emailVerify == "true"),
-          emailTooltip: (item.emailVerify == "true") ?`Verified Email.` : `Not verified yet!` ,
+          emailTooltip: item.emailVerify == "true" ? `Verified Email.` : `Not verified yet!`,
         }
          item.mobileSlot = {
           mobile: item.mobile ?? "",
           mobileVerify: (item.mobileVerify == "true"),
-          mobileTooltip: "Mobile" + ( (item.mobileVerify == "true") ?` is verified.` : ` Not verified yet!` ),
+          mobileTooltip: item.mobileVerify == "true" ?`Verified Mobile` : `Not verified yet!`,
         }
-        
         item.status = {
-          "status": item.isActive ? `Active` : `Inactive`,
-          "verb": item.isActive ? `Suspend` : `Activate`,
-          "_id" : item._id,
-          "name": item.name,
-          "email": item.email
+          status: item.isActive ? `Active` : `Suspended`,
+          verb: item.isActive ? `Suspend` : `Activate`
         }
         return item
       })
       return items
     }
   }
-  
 }
 </script>
-
-<style scoped>
-p {
-  display:inline-block;
-}
-</style>

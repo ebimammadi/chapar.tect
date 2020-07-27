306dd3da11f05c80c17fa6d7622f0cac874f0ceb
ebimammadi <ebi@tadbir.net>
2020-07-27 16:45:31 +0200

    init

---
+++ src/router/routes/app/ticket.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    path: "/tickets",
+    component: () => import("@/views/app/tickets/TicketParent.vue"),
+    meta: {
+      requiresAuth: true,
+      requiresAdmin: true
+    },
+    children: [
+      {
+        path: "",
+        name: "ticket list",
+        component: () => import("@/views/app/tickets/TicketList.vue"),
+        meta: {
+        }
+      },
+      {
+        path: ":ticketId",
+        name: "ticket id public page",
+        component: () => import("@/views/app/tickets/TicketPage.vue"),
+        meta: {
+        }
+      }
+    ]
+  }
+]

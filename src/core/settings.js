88e8cc16eba3b97dadfdb7568f311bc40fe7438d
Ebrahim Mohammadi <19ebmo01@hig.se>
2020-07-27 19:54:36 +0200

    append '/api'

--- src/core/settings.js
+++ src/core/settings.js
@@ -1,6 +1,6 @@
 const APP_SETTINGS = {
   app_name: "chapar.tech",
-  remote_api_base_url: "https://chapar-tech-test.herokuapp.com/", //path to the remote backend
+  remote_api_base_url: "https://chapar-tech-test.herokuapp.com/"+"api", //path to the remote backend
   axios_timeout: 20000, //milliseconds to wait for a backend request's response
   app_version: "1.1.3"
 }

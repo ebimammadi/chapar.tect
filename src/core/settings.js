193d795857b6eafaef55b29e668cb22360904839
ebimammadi <ebi@tadbir.net>
2020-07-31 13:11:58 +0200

    version 1.1.4

--- src/core/settings.js
+++ src/core/settings.js
@@ -1,8 +1,8 @@
 const APP_SETTINGS = {
   app_name: "chapar.tech",
-  remote_api_base_url: "https://chapar-tech-test.herokuapp.com/"+"api", //path to the remote backend
-  axios_timeout: 20000, //milliseconds to wait for a backend request's response
-  app_version: "1.1.3"
+  remote_api_base_url: "https://chapar-tech-test.herokuapp.com/"+"api",
+  axios_timeout: 20000, 
+  app_version: "1.1.4"
 }
 
 export default APP_SETTINGS

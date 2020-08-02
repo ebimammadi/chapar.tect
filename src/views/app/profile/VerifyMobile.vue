5a1263550ed55c34d5ee051ba4b19bf0ec0d9bd2
Ebrahim Mohammadi <19ebmo01@hig.se>
2020-08-02 20:32:35 +0200

    refactor

--- src/views/app/profile/VerifyMobile.vue
+++ src/views/app/profile/VerifyMobile.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-container>
+  <b-container v-if="mobile.length>0">
     <b-row class="mb-3">
       <b-col>
         <h1>Verify Mobile Number</h1>
@@ -10,12 +10,17 @@
         <label for="current-mobile">Mobile Number</label>
         <b-input-group>
           <b-input disabled id="current-mobile" v-model="mobile"></b-input>
+          <b-button v-if="!mobileVerify" @click="sendCodeShowModal" variant="outline-secondary" class="ml-2">
+            Send Code Again?
+          </b-button>
+          <b-button v-if="mobileVerify" variant="outline-secondary" class="ml-2" disabled>✓</b-button>
         </b-input-group>
+        
       </b-col>
     </b-row>
     
-    <b-row class="mb-3"
-      ><b-col>
+    <b-row class="mb-3" v-if="!mobileVerify">
+      <b-col>
         <label for="code">Verification Code</label>
         <b-input-group>
           <b-input
@@ -24,14 +29,14 @@
             placeholder="Enter verification code"
             maxlength="4"
           ></b-input>
+          
           <b-form-invalid-feedback :state="validateCode"
             >{{ code.length > 0 ? validation.code : "" }}
           </b-form-invalid-feedback>
-        </b-input-group></b-col
-      >
+        </b-input-group>
+      </b-col>
     </b-row>
-
-    <b-row>
+    <b-row v-if="!mobileVerify">
       <b-col>
         <b-button variant="outline-success" @click="verifyMobile"
           >Verify Mobile
@@ -41,19 +46,30 @@
         </router-link>
       </b-col>
     </b-row>
+    <modal-confirm 
+      :title="modal.title" 
+      :body="modal.body" 
+      @ok="sendCode()"
+    />
   </b-container>
 </template>
 
 <script>
+import ModalConfirm from "@/components/ModalConfirm"
 import { mapActions } from "vuex"
 import ApiService from "@/core/ApiService"
 
 export default {
+  props: [ 'message', 'variant' ],
+  components: {
+    ModalConfirm,
+  },
   data() {
     return {
-      currentMobile: "",
+      modal: { _id: "", title: "", body:"", function:"" },
       code: "",
       mobile: "",
+      mobileVerify: "",
       validation: {
         code: `The code is not valid!`
       }
@@ -61,6 +77,19 @@ export default {
   },
   methods: {
     ...mapActions(["setAlert"]),
+    sendCodeShowModal(){
+      this.modal.title = `Confirm to Send a new code`
+      this.modal.body = `Are you sure you want to send a code to this number <b>${this.mobile}</b>?`
+      this.$root.$emit( 'bv::show::modal', 'mainModal', '#btnShow')
+    },
+    sendCode(){
+      ApiService.post("/users/send-verification-sms",{ mobile: this.mobile } )
+        .then(response => {
+          this.setAlert({ message: response.data.message, variant: response.data.response_type})
+        })
+        .catch( error => this.setAlert({ message: `Network Error!` }) && console.log(error) )
+    },
+
     verifyMobile() {
       if (!this.validateCode)
         return this.setAlert({ message: this.validation.code })
@@ -69,14 +98,27 @@ export default {
         mobile: this.mobile,
         code: this.code
       })
-        .then(response => { this.setAlert({ message: response.data.message, variant: response.data.response_type}) })
+        .then(response => { 
+          if (response.data.response_type=='success') {
+            this.mobileVerify = true
+          }
+          this.setAlert({ message: response.data.message, variant: response.data.response_type}) 
+        })
         .catch(error => this.setAlert({ message: `Network Error!` }) && console.log(error) )
         .finally(() => {this.code = "" })
     }
   },
   created() {
     ApiService.get("/users/profile-get")
-      .then(response => (this.mobile = response.data.mobile))
+      .then(response => {
+        this.mobile = response.data.mobile
+        this.mobileVerify = response.data.mobileVerify
+        if (this.message) {
+          const message = this.message
+          const variant = this.variant
+          this.setAlert({ message, variant })
+        }
+      })
       .catch(error => this.setAlert({ message: `Network Error!` }) && console.log(error) )
   },
   computed: {

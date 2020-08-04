b742747433ace9c9426bf26ff2f0e6124ee08a2c
Ebrahim Mohammadi <19ebmo01@hig.se>
2020-08-04 14:59:04 +0200

    setting - moved to .env file

--- src/core/settings.js
+++ src/core/settings.js
@@ -1,10 +1,5 @@
-const api_server = "https://chapar-tech-test.herokuapp.com/" 
-//const api_server = "https://api.chapar.tech/"
 const APP_SETTINGS = {
-  app_name: "chapar.tech",
-  remote_api_base_url: api_server+"api",
-  axios_timeout: 20000, 
-  app_version: "1.1.4"
+  app_version: "1.2.6"
 }
 
 export default APP_SETTINGS
--- src/store/index.js
+++ src/store/index.js
@@ -2,15 +2,15 @@ import Vue from "vue"
 import Vuex from "vuex"
 Vue.use(Vuex)
 
+// import setting from "./modules/settings"
 import overlay from "./modules/overlayLoader"
-import setting from "./modules/settings"
 import alertBox from "./modules/alertBox"
 import credentials from "./modules/credentials"
 import routeTo from "./modules/routeTo"
 
 export default new Vuex.Store({
   modules: {
-    setting,
+    // setting,
     overlay,
     credentials,
     alertBox,

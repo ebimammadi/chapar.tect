fd01ae917726da5f11d30a41ce603ed07168937e
Ebrahim Mohammadi <19ebmo01@hig.se>
2020-08-08 20:28:43 +0200

    moment added 'localTimeFormat' directive

--- src/views/app/support/TicketPage.vue
+++ src/views/app/support/TicketPage.vue
@@ -3,9 +3,12 @@
     <b-row>
       <b-col>
         <h5 class="mb-3"><b>{{ ticket.subject }}</b></h5>
-        <h1 class="mb-4">Support Ticket Id: {{$route.params.ticketId}} <span class="bg-light dark p-2"> {{ticket.status | titleize}}</span>  </h1>
-        
-        <p>Associated user: <b>{{ ticket.ownerName }} ({{ticket.ownerEmail}})</b>, 
+        <h1 class="mb-4">
+          Support Ticket Id: {{$route.params.ticketId}} 
+          <span class="bg-light dark p-2"> {{ticket.status | titleize}}</span> 
+          <span class="small" v-if="this.ticket.status =='closed' "> Add a new ticket?</span>
+        </h1>
+        <p v-if="userRole== 'admin'">Associated user: <b>{{ ticket.ownerName }} ({{ticket.ownerEmail}})</b>, 
         <span class="float-right">status: <b>{{ ticket.status || '' | titleize }}</b></span></p>
       </b-col>
     </b-row>
@@ -14,34 +17,48 @@
         <hr/>
         <label for="update" >Add note</label>
         <b-input-group>
-          <b-form-textarea id="update" v-model="updateText" placeholder="Add notes ..." max-rows="12" ></b-form-textarea>
-          <b-form-invalid-feedback :state="validateUpdate" v-if=" updateText!='' "> {{ validation }} </b-form-invalid-feedback>
+          <b-form-textarea id="update" v-model="updateText" placeholder="Add notes ..." max-rows="12" >
+          </b-form-textarea>
+          <b-form-invalid-feedback :state="validateUpdate" v-if=" updateText!='' ">
+            {{ validation }} 
+          </b-form-invalid-feedback>
         </b-input-group>
         <div class="mt-2">
           <b-button @click="updateTicket" variant="outline-success" class="mr-3 mt-2 mb-2">
             Update Ticket
           </b-button>
           <div class="float-right visible-lg">
-            <b-form-checkbox v-model="close" value="closed"  unchecked-value="not-closed" >Close Ticket</b-form-checkbox>
+            <b-form-checkbox v-model="close" value="closed"  unchecked-value="not-closed" >
+              Close Ticket
+            </b-form-checkbox>
           </div>
         </div>
       </b-col>
     </b-row>
-    <b-row class="mb-3" v-for="(update, no) in ticket.updates" :key="update._id"> 
+    <b-row class="mt-3 mb-3" v-for="(update, no) in ticket.updates" :key="update._id"> 
       <b-col>
-        <hr/>
-        <p class="small">#{{ ticket.updates.length - no }}, Updated by {{ update.userName }} ({{update.userEmail}}) <span class="float-right">{{update.date | dateTime}}</span></p>
-        <div v-html="update.text"></div>
+        <!-- <hr/> -->
+        <b-card >
+        <p class="small">#{{ ticket.updates.length - no }}, 
+          Updated by {{ update.userName }} ({{update.userEmail}}) 
+          <span class="float-right">{{update.date | localTimeFormat }}</span>
+          
+        </p>
+        <div style="white-space: pre-line" v-html="update.text"></div>
+        </b-card>
       </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
+import _ from "lodash"
 import { mapActions } from "vuex"
 import ApiService from "@/core/ApiService"
+import JwtService from "@/core/JwtService"
 
 export default {
+  props: [ 'message', 'response_type' ],
   data() {
     return {
       ticket: { },
@@ -52,22 +69,36 @@ export default {
   },
   methods: {
     ...mapActions(["setAlert"]),
-    updateTicket(){
-      //validate the update,
-      //handle the close
-      //submit to the backend
-      //update the updates array 
-      //update the close button
-      //if closed close the update box, show add another ticket?! (new tickat page link)
-    
-
+    updateTicket() {
+      if (!this.validateUpdate) return this.setAlert(this.validation)
+      const payload = {
+        ticketId: this.ticket.ticketId,
+        ownerEmail: this.ticket.ownerEmail,
+        status: (this.close !== 'closed' ) ? this.ticket.status.toLowerCase().replace(' ','-') : 'closed',
+        text: this.updateText
+      }
+      ApiService.post(`/tickets/ticket-update`, payload)
+        .then(response => {
+         if (response.data.response_type == 'success') {
+           this.ticket.updates.unshift(response.data.update) 
+           this.ticket.status = (this.close !== 'closed' ) ? this.ticket.status : 'closed'
+           this.updateText = ''
+         } 
+         this.setAlert( _.pick(response.data, ['message', 'response_type']))
+        })
+        .catch( error => this.setAlert( { message: error.data.message } ))
     }
   },
   created() {
     ApiService.get(`/tickets/ticket-get/${this.$route.params.ticketId}`)
       .then(response => {
         this.ticket = response.data
-        if (this.ticket.updates) this.ticket.updates.reverse() 
+        if (this.message) {
+          const message = this.message
+          const response_type = this.response_type
+          this.setAlert({ message, response_type })
+        }
+        //if (this.ticket.updates) this.ticket.updates.reverse() 
       })
       .catch( error => this.setAlert( { message: error.data.message } ))
   },
@@ -75,7 +106,12 @@ export default {
     validateUpdate() {
       if (this.updateText.length>10) return true
       return false
+    },
+    userRole() {
+      const { userRole } = JwtService.decodeToken()
+      return userRole
     }
   }
+  
 }
 </script>

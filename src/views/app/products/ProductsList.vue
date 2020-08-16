1fa8f64b98fcdc29ce6161f11d286a86a7dfde6e
ebimammadi <ebi@tadbir.net>
2020-08-16 14:36:39 +0200

    refactor

--- src/views/app/products/ProductsList.vue
+++ src/views/app/products/ProductsList.vue
@@ -23,11 +23,11 @@
             class="mb-2 mr-sm-3 mb-sm-0"
             debounce="1000"
           ></b-input>
-          <label class="mr-sm-1" for="inline-form-custom-select-pref">Status</label>
+          <label class="mr-sm-1" for="inline-form-custom-select-pref">Publish Status</label>
           <b-form-select
-            v-model="status"
+            v-model="publishStatus"
             class="mb-2 mr-sm-3 mb-sm-0"
-            :options="statusOptions"
+            :options="publishStatusOptions"
             :value="null"
           ></b-form-select>
           <div class="mb-2 mr-sm-3 mb-sm-0">Total: <b>{{ productsRaw.count }}</b></div>
@@ -37,94 +37,151 @@
           striped
           hover
           stacked="sm"
-          :items="tickets"
+          :items="products"
           :fields="fields"
           :class="'table'"
         >
-          <template v-slot:cell(ticketId)="data">
-            <router-link
+          <template v-slot:cell(name)="data">
+            {{ data.item.name | titleize}} <router-link
               variant="primary" 
               v-b-tooltip.hover title="View Ticket Details"
-              :to="{ name: 'ticket page', params: { ticketId: data.item.ticketId } }"
+              :to="{ name: 'edit product', params: { productId: data.item._id } }"
             >
-            {{ data.item.ticketId }}</router-link>
+            {{ data.item.slug }}</router-link>
+            {{ data.item.description.slice(0,10)+'...' | titleize}} 
           </template>
-          <template v-slot:cell(status)="data">
-            {{ data.item.status | titleize}} 
+          <template v-slot:cell(ownerId)="data">
+            {{ data.item.ownerSlug | titleize}} 
           </template>
-          <template v-slot:cell(subject)="data">
-            <router-link
-              variant="primary" 
-              v-b-tooltip.hover title="View Ticket Details"
-              :to="{ name: 'ticket page', params: { ticketId: data.item.ticketId } }"
-            >
-            {{ data.item.subject }}</router-link>
-          </template>
-          <template v-slot:cell(ownerEmail)="data">
-            {{ data.item.ownerName }}, {{ data.item.ownerEmail}} 
-            <router-link 
-              variant="primary" 
-              v-b-tooltip.hover title="View User Profile"
-              :to="{ name: 'user profile', params: { user: data.item.ownerEmail } }"
-            >
-              <b-icon-person />                  
-            </router-link>
-          </template>
-          <template v-slot:cell(updated_at)="data">
-            {{ data.item.updated_at | localTimeFormat }} 
+          <template v-slot:cell(publishStatus)="data">
+            {{ data.item.publishStatus.toString() | titleize}} 
+            <b-button @click="productPublishModal(data.item._id)"
+              variant="link"
+              size="sm"
+              :id="`publish`+ data.item._id" 
+              >
+              <b-icon-pencil />
+            </b-button>
+            <b-button @click="productDeleteModal(data.item._id)"
+              variant="danger link"
+              size="sm"
+              :id="`delete`+ data.item._id" 
+              >
+              <b-icon-trash />
+            </b-button>
+            <b-tooltip :target="`publish`+ data.item._id">
+              {{ data.item.publishStatus.toString() == `true`? `Suspend product` : `Publish product` }}
+            </b-tooltip>
+            <b-tooltip :target="`delete`+ data.item._id">
+              Delete product
+            </b-tooltip>
           </template>
           <template v-slot:cell(date)="data">
             {{ data.item.date | localTimeFormat }} 
           </template>
+          
         </b-table>
-        <!-- <modal-confirm :title="modal.title" 
+        <modal-confirm :title="modal.title" 
           :body="modal.body" 
           :_id="modal._id" 
           @ok="handleConfirmOk(modal.function)"
-        /> -->
+        />
       </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
+import ModalConfirm from "@/components/ModalConfirm"
 import { mapActions } from "vuex"
 import ApiService from "@/core/ApiService"
 
 export default {
+  components: {
+    ModalConfirm,
+  },
   data(){
     return {
-      productsRaw: { tickets: [] },
+      modal: { _id: "", title: "", body:"", function:"" },
+      productsRaw: { products: [] },
       fields: [
-        { key: "ticketId" , label: "Id" },  
-        { key: "subject" , label: "Subject" },  
-        { key: "ownerEmail", label: "User" },
-        { key: "status" , label: "Status" },  
-        { key: "date", label: "Creation Time" },
-        { key: "updated_at", label: "Update Time" }
+        { key: "name" , label: "Product" },  
+        { key: "ownerId", label: "Supplier" },
+        { key: "publishStatus" , label: "Published" },  
+        { key: "date", label: "Date" },
       ],
-      statusOptions: [
+      publishStatusOptions: [
         { text: 'All', value: '' }, 
-        { text: 'Open', value: 'open' },
-        { text: 'Closed', value: 'closed' },
-        { text: 'Customer Action', value: 'action-required' }
+        { text: 'Published', value: 'true' },
+        { text: 'Unpublished', value: 'false' },
       ]
     }
   },
   methods: {
     ...mapActions(["setAlert"]),
-    invokeTickets() {
-      ApiService.get(`/app-tickets/ticket-list?page=${this.currentPage}&search=${this.search}&status=${this.status}`)
+    handleConfirmOk(function_name) {
+      this[function_name](this.modal._id)
+    },
+    productPublishModal(_id) {
+      const item = this.productsRaw.products.find( item => item._id === _id)
+      const action = item.publishStatus.toString() == 'true' ? `suspend` : `publish`
+      this.modal.title = item.publishStatus.toString() == 'true' ? `Suspend Product` : `Publish Product`
+      this.modal.body = `Are you sure to ${action} the product <b>${item.name} (${item.slug})</b>?`
+      this.modal._id = _id
+      this.modal.function = "productPublish"
+      this.$root.$emit( 'bv::show::modal', 'mainModal', '#btnShow')
+    }, 
+    productPublish(){
+      const _id = this.modal._id
+      const item = this.productsRaw.products.find( item => item._id === _id )
+      const request = item.publishStatus.toString() == 'true' ? '/app-products/product-suspend' : '/app-products/product-publish'
+      ApiService.post(request, { _id })
+        .then(response => {
+          this.setAlert({message: response.data.message, variant: response.data.response_type})
+          if (response.data.response_type === "success"){
+            this.productsRaw.products.map(item => {
+              if (item._id === _id) item.publishStatus = ! (item.publishStatus.toString() == 'true')
+              return item
+            })
+          }
+        })
+        .catch( error => this.setAlert( { message: error.data.message } ))
+    },
+    productDeleteModal(_id) {
+      const item = this.productsRaw.products.find( item => item._id === _id)
+      this.modal.title = `Delete Product`
+      this.modal.body = `Are you sure to delete the product <b>${item.name} (${item.slug})</b>?<br/>
+        Note: The product won't be available no longer, and this process cannot be undone.`
+      this.modal._id = _id
+      this.modal.function = "productDelete"
+      this.$root.$emit( 'bv::show::modal', 'mainModal', '#btnShow')
+    }, 
+    productDelete(){
+      const _id = this.modal._id
+      ApiService.post('/app-products/product-delete', { _id })
+        .then(response => {
+          this.setAlert({message: response.data.message, variant: response.data.response_type})
+          if (response.data.response_type === "success"){
+            this.productsRaw.products.map(item => {
+              if (item._id === _id) return item
+            })
+          }
+        })
+        .catch( error => this.setAlert( { message: error.data.message } ))
+    },
+    invokeProducts() {
+      this.productsRaw.perPage = null
+      ApiService.get(`/app-products/product-list?page=${this.currentPage}&search=${this.search}&publishStatus=${this.publishStatus}`)
       .then(response => this.productsRaw = response.data )
       .catch( error => this.setAlert({ message: error.data.message }) )      
     }
   },
   created() {
-    this.invokeTickets()
+    this.invokeProducts()
   },
   computed: {
-    tickets() {
-      return this.productsRaw.tickets
+    products() {
+      return this.productsRaw.products
     },
     currentPage: {
       get() { 
@@ -132,7 +189,7 @@ export default {
       },
       set(newPage) {
         this.$router.push({ query: { ...this.$route.query, page: newPage }}).catch(()=>{})
-        this.invokeTickets()
+        this.invokeProducts()
       }
     },
     search: {
@@ -141,16 +198,16 @@ export default {
       },
       set(newSearch) {
         this.$router.push({ query: { ...this.$route.query, search: newSearch }}).catch(()=>{})
-        this.invokeTickets()
+        this.invokeProducts()
       }
     },
-    status: {
+    publishStatus: {
       get() {
-        return this.$route.query.status || ''
+        return this.$route.query.publishStatus || ''
       },
       set(newStatus) {
-        this.$router.push({ query: { ...this.$route.query, status: newStatus  }}).catch(()=>{})
-        this.invokeTickets()
+        this.$router.push({ query: { ...this.$route.query, publishStatus: newStatus  }}).catch(()=>{})
+        this.invokeProducts()
       }
     },
   }

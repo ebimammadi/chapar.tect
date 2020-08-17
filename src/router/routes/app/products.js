ce50171aac96338f6c454e6ff019dc33f95b0b3f
ebimammadi <ebi@tadbir.net>
2020-08-17 19:42:30 +0200

    requiresSupplier added

--- src/router/routes/app/products.js
+++ src/router/routes/app/products.js
@@ -3,30 +3,25 @@ export default [
     path: "products",
     component: () => import("@/views/app/products/ProductsParent.vue"),
     meta: {
-      requiresAuth: true
+      requiresAuth: true,
+      requiresSupplier: true
     },
     children: [
       {
         path: "",
         name: "product list",
         component: () => import("@/views/app/products/ProductsList.vue"),
-        meta: {
-        }
       },
       {
         path: "id/:_id",
         name: "edit product",
         component: () => import("@/views/app/products/ProductsNewPage.vue"),
-        meta: {
-        },
         props: true
       },
       {
         path: "new",
         name: "new product",
         component: () => import("@/views/app/products/ProductsNewPage.vue"),
-        meta: {
-        }
       }
     ]
   }
